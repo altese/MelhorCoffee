f7d47a9a5b5d70aae975d22a9bf0cede0ab19bf5
김한별 <whdlgksquf5@naver.com>
2022-10-02 16:16:14 +0900

    Hanbyeol (#8)
    * hanbyeol1
    
    * qwer
    
    * 1234
    
    * 1111
    
    * dto
    
    * helpdto
    
    * 수정본
    
    * 스태틱커스터머아이디
    
    Co-authored-by: kimjn132 <104209276+kimjn132@users.noreply.github.com>

---
+++ src/com/javalec/base/Static_CustomerId.java
@@ -0,0 +1,21 @@
+package com.javalec.base;
+
+public class Static_CustomerId {
+
+	public static String customer_id;
+	
+	public Static_CustomerId() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static String getCustomer_id() {
+		return customer_id;
+	}
+	public static void setCustomer_id(String customer_id) {
+		Static_CustomerId.customer_id = customer_id;
+	}
+	
+	
+	
+}
