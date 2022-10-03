0f8033a610e005b3c30c95e6ba8e699c08c26894
김한별 <whdlgksquf5@naver.com>
2022-10-03 14:35:25 +0900

    Hanbyeol (#15)
    * hanbyeol1
    
    * qwer
    
    * 1234
    
    * 1111
    
    * dto
    
    * helpdto
    
    * 수정본
    
    * 스태틱커스터머아이디
    
    * orders 구매, 구매 후 product_stock 구매한 개수만큼 차감
    
    * Dto수정본
    
    * 이전페이지 이동등등 추가

--- src/com/javalec/base/Help.java
+++ src/com/javalec/base/Help.java
@@ -24,6 +24,10 @@ public class Help extends JDialog {
 	private JLabel lblproductPrice;
 	private JLabel lblProductPrice2;
 	private JLabel lblProductId;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JButton btnBackPage;
 	
 
 	/**
@@ -36,6 +40,7 @@ public class Help extends JDialog {
 					Help dialog = new Help();
 					dialog.frmDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 					dialog.frmDialog.setVisible(true);
+					dialog.frmDialog.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,7 +61,7 @@ public class Help extends JDialog {
 	 */
 	public Help() {
 		setTitle("Help");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 358, 300);
 		getContentPane().setLayout(null);
 		getContentPane().add(getLblProductName());
 		getContentPane().add(getBtnOrder());
@@ -64,13 +69,17 @@ public class Help extends JDialog {
 		getContentPane().add(getLblproductPrice());
 		getContentPane().add(getLblProductPrice2());
 		getContentPane().add(getLblProductId());
+		getContentPane().add(getLblNewLabel());
+		getContentPane().add(getLblNewLabel_1());
+		getContentPane().add(getLblNewLabel_2());
+		getContentPane().add(getBtnBackPage());
 
 	}
 
 	private JLabel getLblProductName() {
 		if (lblProductName == null) {
 			lblProductName = new JLabel("제품명");
-			lblProductName.setBounds(161, 70, 103, 15);
+			lblProductName.setBounds(175, 38, 103, 15);
 		}
 		return lblProductName;
 	}
@@ -84,7 +93,7 @@ public class Help extends JDialog {
 					
 				}
 			});
-			btnOrder.setBounds(318, 230, 91, 23);
+			btnOrder.setBounds(220, 212, 91, 30);
 		}
 		return btnOrder;
 	}
@@ -92,16 +101,16 @@ public class Help extends JDialog {
 	private JLabel getLblproductPrice() {
 		if (lblproductPrice == null) {
 			lblproductPrice = new JLabel("가격");
-			lblproductPrice.setBounds(161, 147, 103, 23);
+			lblproductPrice.setBounds(175, 122, 103, 23);
 		}
 		return lblproductPrice;
 	}
 	
 	private JLabel getLblProductPrice2() {
 		if (lblProductPrice2 == null) {
-			lblProductPrice2 = new JLabel("New label");
+			lblProductPrice2 = new JLabel("");
 			lblProductPrice2.setVisible(false);
-			lblProductPrice2.setBounds(326, 70, 50, 15);
+			lblProductPrice2.setBounds(261, 202, 50, 15);
 		}
 		return lblProductPrice2;
 	}
@@ -119,20 +128,58 @@ public class Help extends JDialog {
 			});
 			
 			cbStock.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9"}));
-			cbStock.setBounds(161, 98, 103, 23);
+			cbStock.setBounds(175, 77, 103, 23);
 		}
 		return cbStock;
 	}
 	
 	private JLabel getLblProductId() {
 		if (lblProductId == null) {
-			lblProductId = new JLabel("New label");
-			lblProductId.setBounds(326, 116, 50, 15);
+			lblProductId = new JLabel("");
+			lblProductId.setBounds(261, 227, 50, 15);
 			lblProductId.setVisible(false);
 		}
 		return lblProductId;
 	}
 	
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("제품명 :");
+			lblNewLabel.setBounds(113, 38, 50, 15);
+		}
+		return lblNewLabel;
+	}
+	
+	private JLabel getLblNewLabel_1() {
+		if (lblNewLabel_1 == null) {
+			lblNewLabel_1 = new JLabel("수량 :");
+			lblNewLabel_1.setBounds(113, 81, 50, 15);
+		}
+		return lblNewLabel_1;
+	}
+	
+	private JLabel getLblNewLabel_2() {
+		if (lblNewLabel_2 == null) {
+			lblNewLabel_2 = new JLabel("가격 :");
+			lblNewLabel_2.setBounds(113, 126, 50, 15);
+		}
+		return lblNewLabel_2;
+	}
+	
+	private JButton getBtnBackPage() {
+		if (btnBackPage == null) {
+			btnBackPage = new JButton("이전 페이지");
+			btnBackPage.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					backPage();
+				}
+			});
+			btnBackPage.setBounds(105, 212, 103, 30);
+		}
+		return btnBackPage;
+	}
+	
+	// ---------------------------------------------------------------------------------------
 	
 	public void productOrderList(int wkSequence) {
 		
@@ -158,6 +205,13 @@ public class Help extends JDialog {
 		if(check == 1) {
 			JOptionPane.showMessageDialog(null, "상품 구매가 완료 되었습니다.");
 			dao.productStockOut(product_id, product_stock);
+			
+			// 10/03 한별수정 - 상품 구매 후 신발 리스트 출력 페이지로 이동
+			productSelectList.main(null);
+			boolean help = new Help() == null;
+			setVisible(help);
+			// ------------------------------------------
+			
 		} else {
 			JOptionPane.showMessageDialog(null, "DB 작업중 문제가 발생했습니다. \n행정실로 문의 하세요!");
 		}
@@ -167,24 +221,11 @@ public class Help extends JDialog {
 		lblproductPrice.setText(Integer.toString(Integer.parseInt((String)lblProductPrice2.getText()) * Integer.parseInt((String)cbStock.getSelectedItem())));
 		
 	}
-
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void backPage() {
+		productSelectList.main(null);
+		boolean help = new Help() == null;
+		setVisible(help);
+	}
 	
 } // End Line
